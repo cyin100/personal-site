6d1b543bd32ae33d0400de7ab8f032832999096e
cyin100 <46613498+cyin100@users.noreply.github.com>
2022-01-22 01:28:04 -0800

    functional resume button

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,14 @@ function Header () {
                 <span className="bg-[url('images\\Conner1.jpg')] bg-contain bg-no-repeat bg-center mx-auto rounded-full p-20"></span>
             </div>
 
-            <button class="flex mx-auto bg-transparent border-4 border-white text-white transition duration-150 ease-out hover:ease-in hover:bg-white hover:text-black font-bold py-2 px-4 rounded-md mb-10">
+            <a href="https://drive.google.com/file/d/1nxgKt0AkyR_X5V2arYtHB8DIfNb_fOIf/view?usp=sharing" class="flex w-24 mx-auto bg-transparent border-4 border-white text-white transition duration-150 ease-out hover:ease-in hover:bg-white hover:text-black font-bold py-2 px-4 rounded-md mb-10">
                 Resume
-            </button>
+            </a>
 
             <div className = 'flex bg-white w-full mb-10 shadow-md rounded-lg'>
-                <div className = 'p-5 mx-auto'>
+                <div className = 'px-2 py-5 mx-auto'>
                     <h1 className='md:text-2xl text-center'>Hi, I'm <b>Conner Yin</b>.</h1>
-                    <p className='mt-4 text-center mb-10'>I'm a programmer and musician studying Computer Science and Engineering at Santa Clara University. Scroll down to see what I've been up to!</p>
+                    <p className='mt-4 text-center mb-8'>I'm a programmer and musician studying Computer Science and Engineering at Santa Clara University. Scroll down to see what I've been up to!</p>
                     <div className = 'grid grid-cols-5 mx-auto'>
                         <div></div>
                         <a href="https://github.com/cyin100" className="basis-1/6 bg-[url('images\\icons\\github.png')] bg-contain bg-no-repeat bg-center mx-auto rounded-full p-5 transition duration-150 ease-out hover:ease-in hover:animate-pulse"></a>
--- src/components/ProjectsMain.js
+++ src/components/ProjectsMain.js
@@ -4,7 +4,7 @@ import projects from '../data/projects.json';
 function ProjectsMain () {
     return (
         <div>
-            <div className = 'bg-gradient-to-r from-pink-500 to-orange-500 flex bg-white w-40 mx-auto mb-5 shadow-md rounded-lg p-3'>
+            <div className = 'mt-8 bg-gradient-to-r from-pink-500 to-orange-500 flex bg-white w-40 mx-auto mb-5 shadow-md rounded-lg p-3'>
                 <h1 className='md:text-2xl font-bold mx-auto text-white mr-2'>Projects</h1>
                 <div className="bg-[url('images\\icons\\github.png')] bg-contain bg-no-repeat bg-center w-full"></div>
             </div>
